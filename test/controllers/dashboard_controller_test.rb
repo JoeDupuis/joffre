f8ed8a63f67b350fd99e68ab757b4680afabfa00
Joé Dupuis <joe@dupuis.io>
2025-06-10 19:18:45 -0700

    Merge dashboard controller tests into single test
    - Combine unauthenticated and authenticated tests into one
    - Test shows full flow: redirect when not signed in, then success after sign in

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,12 +1,10 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should redirect to login when not authenticated" do
+  test "should redirect to login when not authenticated then succeed when authenticated" do
     get root_url
     assert_redirected_to new_session_path
-  end
 
-  test "should get index when authenticated" do
     sign_in_as(users(:one))
     get root_url
     assert_response :success
