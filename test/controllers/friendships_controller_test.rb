795eaf46dca7d44085356c670d6f5a2d6a40f896
Joé Dupuis <joe@dupuis.io>
2025-06-10 22:34:20 -0700

    Add tests for accepting and declining friend requests
    - Test accepting friend request creates reciprocal friendship
    - Test declining friend request removes the friendship
    - Test canceling sent friend request
    - Use no_friends fixture to avoid conflicts with existing friendships

--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -32,6 +32,44 @@ class FriendshipsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to friendships_url
   end
 
+  test "should accept friend request" do
+    no_friends = users(:no_friends)
+    friendship = Friendship.create!(user: no_friends, friend: @user, pending: true)
+    
+    patch friendship_url(friendship)
+    assert_redirected_to friendships_url
+    
+    friendship.reload
+    assert_not friendship.pending
+    
+    reciprocal = Friendship.find_by(user: @user, friend: no_friends)
+    assert reciprocal
+    assert_not reciprocal.pending
+  end
+
+  test "should decline friend request" do
+    no_friends = users(:no_friends)
+    friendship = Friendship.create!(user: no_friends, friend: @user, pending: true)
+    
+    assert_difference("Friendship.count", -1) do
+      delete friendship_url(friendship)
+    end
+    
+    assert_redirected_to friendships_url
+  end
+
+  test "should cancel sent friend request" do
+    no_friends = users(:no_friends)
+    friendship = Friendship.create!(user: @user, friend: no_friends, pending: true)
+    
+    assert_difference("Friendship.count", -1) do
+      delete friendship_url(friendship)
+    end
+    
+    assert_redirected_to friendships_url
+  end
+
+
   test "should redirect to login when not authenticated for index" do
     sign_out
     get friendships_url
