02a2d5f36b76f436e391bd68b7e05346f50a9ac7
Joé Dupuis <joe@dupuis.io>
2025-06-10 19:33:44 -0700

    Fix migration order by renaming AddNameToUsers timestamp
    The AddNameToUsers migration needs to run after CreateUsers.
    Renamed from 20250611014802 to 20250611020401 to ensure correct order.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_06_11_014802) do
+ActiveRecord::Schema[8.1].define(version: 2025_06_11_020401) do
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "ip_address"
