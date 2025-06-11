2cd90e53c99457461de3fe285845fc992be54d0d
Joé Dupuis <joe@dupuis.io>
2025-06-10 22:22:02 -0700

    Refactor destroy action to use OR query instead of rescue
    - Use a single query with OR to find friendship in either direction
    - Check friend_id to determine if it's a received or sent request
    - Remove ActiveRecord::RecordNotFound rescue pattern

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -41,12 +41,14 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    @friendship = Current.user.received_friend_requests.find(params[:id])
-    @friendship.decline!
-    redirect_to friendships_path, notice: "Friend request declined"
-  rescue ActiveRecord::RecordNotFound
-    @friendship = Current.user.sent_friend_requests.find(params[:id])
-    @friendship.destroy
-    redirect_to friendships_path, notice: "Friend request cancelled"
+    @friendship = Current.user.received_friend_requests.or(Current.user.sent_friend_requests).find(params[:id])
+    
+    if @friendship.friend_id == Current.user.id
+      @friendship.decline!
+      redirect_to friendships_path, notice: "Friend request declined"
+    else
+      @friendship.destroy
+      redirect_to friendships_path, notice: "Friend request cancelled"
+    end
   end
 end
